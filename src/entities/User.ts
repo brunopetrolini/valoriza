83cb23fd4131687b60629b958b58ba7d807e9768
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-06-22 20:31:26 -0300

    feat: add User entity

---
+++ src/entities/User.ts
@@ -0,0 +1,27 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm'
+import { v4 as uuidv4 } from 'uuid'
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  readonly id: string
+
+  @Column()
+  name: string
+
+  @Column()
+  email: string
+
+  @Column()
+  admin: boolean
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+
+  constructor () {
+    if (!this.id) this.id = uuidv4()
+  }
+}
