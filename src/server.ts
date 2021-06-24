7704a929d0f7443be1c73a9b413d9ea873768c19
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-06-23 21:35:49 -0300

    feat: add error middleware

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
-import express from 'express'
+import 'express-async-errors'
+import express, { NextFunction, Request, Response } from 'express'
 
 import { router } from './routes'
 
@@ -10,5 +11,13 @@ const app = express()
 app.use(express.json())
 
 app.use(router)
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof Error) {
+    return response.status(400).json({ error: error.message })
+  }
+
+  return response.status(500).json({ status: 'UnexpectedError', message: 'Internal Server Error' })
+})
 
 app.listen(3333, () => console.log('Server running at port 3333'))
