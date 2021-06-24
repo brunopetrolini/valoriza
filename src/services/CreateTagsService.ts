52217ec46e1916fda976913b553b7935cb5a6490
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-06-23 21:56:00 -0300

    feat: add CreateTagsService

---
+++ src/services/CreateTagsService.ts
@@ -0,0 +1,21 @@
+import { getCustomRepository } from 'typeorm'
+import { Tag } from '../entities/Tag'
+import { TagsRepository } from '../repositories/TagsRepository'
+
+export class CreateTagsService {
+  async execute (name: string): Promise<Tag> {
+    const tagsRepository = getCustomRepository(TagsRepository)
+
+    if (!name) throw new Error('Invalid name')
+
+    const tagExists = await tagsRepository.findOne({ name })
+
+    if (!tagExists) throw new Error('Tag name already exists')
+
+    const tag = tagsRepository.create({ name })
+
+    const result = await tagsRepository.save(tag)
+
+    return result
+  }
+}
