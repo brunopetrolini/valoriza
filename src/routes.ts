5def0f8ecc11a931d33295df1b2ff10b75d3ecfc
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-06-23 21:59:42 -0300

    feat: add tags route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
+import { CreateTagsController } from './controllers/CreateTagsController'
 import { CreateUserController } from './controllers/CreateUserController'
 
 const router = Router()
 
 const createUserController = new CreateUserController()
 router.post('/users', createUserController.handle)
+
+const createTagsController = new CreateTagsController()
+router.post('/tags', createTagsController.handle)
 
 export { router }
