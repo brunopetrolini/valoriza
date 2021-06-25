25237035072bdbeb372fbc344250fd4e3dfe021b
Bruno Petrolini <brunopetrolini@hotmail.com>
2021-06-25 10:28:30 -0300

    fix: add ensureAdmin  middleware

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,9 +1,16 @@
 import { NextFunction, Request, Response } from 'express'
+import { getCustomRepository } from 'typeorm'
+import { UsersRepository } from '../repositories/UsersRepository'
 
-export function ensureAdmin (request: Request, response: Response, next: NextFunction): any {
-  const isAdmin = true
+export async function ensureAdmin (request: Request, response: Response, next: NextFunction): Promise<any> {
+  const { userId } = request
 
-  if (isAdmin) return next()
+  const usersRepository = getCustomRepository(UsersRepository)
 
-  return response.status(401).json({ error: 'Unauthorized' })
+  const user = await usersRepository.findOne(userId)
+  if (user?.isAdmin) return next()
+
+  return response.status(401).json({
+    error: 'Unauthorized'
+  })
 }
